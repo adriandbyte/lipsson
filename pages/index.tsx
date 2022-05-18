8aa6b8cd588cab6a13a2b22bd45d128098d999a8
adrian <adrian.dominguez.gomezz@gmail.com>
2022-05-17 22:27:50 -0500

    change the index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import type { NextPage } from "next";
+import React from 'react'
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+    return (
+        <h1 className="text-3xl font-bold underline">Aplicacion de Lipsson</h1>
+    )
+}
 
-export default Home;
+export default Home
